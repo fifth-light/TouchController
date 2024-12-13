89f07c95cc47e5ea0fbe00c41904c433d7a74c82
fifth_light <liu-yu-jie@foxmail.com>
2024-12-14 01:37:50 +0800

    发布 proxy-client 库到 Maven Central 中

--- proxy-client/build.gradle.kts
+++ proxy-client/build.gradle.kts
@@ -1,17 +1,47 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
-    `maven-publish`
+    signing
     alias(libs.plugins.jetbrains.kotlin.jvm)
     alias(libs.plugins.jetbrains.kotlin.serialization)
+    alias(libs.plugins.maven.publish)
 }
 
 version = "0.0.1"
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            afterEvaluate {
-                from(components["java"])
+signing {
+    useGpgCmd()
+    sign(configurations.archives.get())
+}
+
+mavenPublishing {
+    coordinates("top.fifthlight.touchcontroller", "proxy-client", version.toString())
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    pom {
+        name.set("Proxy client library of TouchController")
+        description.set("A library to connect to the Minecraft mod TouchController.")
+        inceptionYear.set("2024")
+        url.set("https://github.com/fifth-light/TouchController")
+        licenses {
+            license {
+                name.set("GNU Lesser General Public License v3.0")
+                url.set("https://www.gnu.org/licenses/lgpl-3.0.html")
+                distribution.set("https://www.gnu.org/licenses/lgpl-3.0.html")
             }
+        }
+        developers {
+            developer {
+                id.set("fifth_light")
+                name.set("fifth_light")
+                url.set("https://github.com/fifth-light/")
+            }
+        }
+        scm {
+            url.set("https://github.com/fifth-light/TouchController")
+            connection.set("scm:git:https://github.com/fifth-light/TouchController.git")
+            developerConnection.set("scm:git:ssh://git@github.com:fifth-light/TouchController.git")
         }
     }
 }
--- proxy-server/build.gradle.kts
+++ proxy-server/build.gradle.kts
@@ -1,23 +1,8 @@
 plugins {
-    `maven-publish`
     alias(libs.plugins.jetbrains.kotlin.jvm)
 }
 
 version = "0.0.1"
-
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            afterEvaluate {
-                from(components["java"])
-            }
-        }
-    }
-}
-
-base {
-    archivesName = "TouchController-Proxy-Server"
-}
 
 dependencies {
     implementation(project(":proxy-client"))
