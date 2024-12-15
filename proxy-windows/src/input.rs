ea72dd9de5007bd709ed0836029b8ea7b2933118
fifth_light <liu-yu-jie@foxmail.com>
2024-12-15 15:53:27 +0800

    改进 Windows 上的触摸处理

--- proxy-windows/src/input.rs
+++ proxy-windows/src/input.rs
@@ -9,10 +9,16 @@ use std::{
 
 use proxy_protocol::ProxyMessage;
 use windows::Win32::{
-    Foundation::{HINSTANCE, HWND, LPARAM, LRESULT, POINT, RECT, WPARAM},
+    Foundation::{BOOL, HINSTANCE, HWND, LPARAM, LRESULT, POINT, RECT, WPARAM},
     Graphics::Gdi::ClientToScreen,
     System::Threading::GetCurrentThreadId,
     UI::{
+        Controls::{
+            SetWindowFeedbackSetting, FEEDBACK_GESTURE_PRESSANDTAP,
+            FEEDBACK_TOUCH_CONTACTVISUALIZATION, FEEDBACK_TOUCH_DOUBLETAP,
+            FEEDBACK_TOUCH_PRESSANDHOLD, FEEDBACK_TOUCH_RIGHTTAP, FEEDBACK_TOUCH_TAP,
+            FEEDBACK_TYPE,
+        },
         Input::Touch::{
             GetTouchInputInfo, RegisterTouchWindow, HTOUCHINPUT, TOUCHEVENTF_DOWN,
             TOUCHEVENTF_MOVE, TOUCHEVENTF_UP, TOUCHINPUT, TWF_WANTPALM,
@@ -30,6 +36,7 @@ pub static EVENT_QUEUE: Mutex<VecDeque<ProxyMessage>> = Mutex::new(VecDeque::new
 pub enum InitializeError {
     RegisterTouchWindow(windows::core::Error),
     SetWindowsHookExW(windows::core::Error),
+    SetWindowFeedbackSetting,
 }
 
 impl Display for InitializeError {
@@ -41,15 +48,50 @@ impl Display for InitializeError {
             InitializeError::SetWindowsHookExW(error) => {
                 write!(f, "SetWindowsHookExW() failed: {:?}", error)
             }
+            InitializeError::SetWindowFeedbackSetting => {
+                write!(f, "SetWindowFeedbackSetting() failed")
+            }
         }
     }
 }
 
 impl Error for InitializeError {}
 
+pub fn disable_feedback(handle: HWND, feedback: FEEDBACK_TYPE) -> Result<(), ()> {
+    let mut enabled: BOOL = BOOL(0);
+    let enabled: *mut BOOL = &mut enabled;
+    if unsafe {
+        SetWindowFeedbackSetting(
+            handle,
+            feedback,
+            0,
+            size_of::<BOOL>() as u32,
+            Some(enabled as *mut c_void),
+        )
+    }
+    .0 == 0
+    {
+        Err(())
+    } else {
+        Ok(())
+    }
+}
+
 pub fn init(handle: HWND) -> Result<(), InitializeError> {
     unsafe { RegisterTouchWindow(handle, TWF_WANTPALM) }
         .map_err(InitializeError::RegisterTouchWindow)?;
+
+    for feedback in [
+        FEEDBACK_TOUCH_CONTACTVISUALIZATION,
+        FEEDBACK_TOUCH_TAP,
+        FEEDBACK_TOUCH_DOUBLETAP,
+        FEEDBACK_TOUCH_PRESSANDHOLD,
+        FEEDBACK_TOUCH_RIGHTTAP,
+        FEEDBACK_GESTURE_PRESSANDTAP,
+    ] {
+        disable_feedback(handle, feedback)
+            .map_err(|_| InitializeError::SetWindowFeedbackSetting)?;
+    }
 
     let thread_id = unsafe { GetCurrentThreadId() };
     unsafe {
@@ -94,13 +136,30 @@ impl Display for EventError {
 
 impl Error for EventError {}
 
-static POINTER_STATE: Mutex<Option<(HashMap<u32, u32>, u32)>> = Mutex::new(None);
+#[derive(Debug)]
+struct PointerState {
+    tick: u64,
+    next_id: u32,
+    id_map: HashMap<u32, (u32, u64)>,
+}
+
+impl Default for PointerState {
+    fn default() -> Self {
+        Self {
+            tick: 0,
+            next_id: 1,
+            id_map: HashMap::new(),
+        }
+    }
+}
+
+static POINTER_STATE: Mutex<Option<PointerState>> = Mutex::new(None);
 
 fn handle_touch_event(message: &CWPSTRUCT) -> Result<(), EventError> {
     let pointers_count = low_word(message.wParam.0);
     let pointers_handle = message.lParam.0;
 
-    let mut pointers = vec![TOUCHINPUT::default(); pointers_count];
+    let mut pointers = vec![TOUCHINPUT::default(); pointers_count.min(16)];
 
     unsafe {
         GetTouchInputInfo(
@@ -125,21 +184,41 @@ fn handle_touch_event(message: &CWPSTRUCT) -> Result<(), EventError> {
 
     let mut queue = EVENT_QUEUE.lock().unwrap();
     let mut pointers_state = POINTER_STATE.lock().unwrap();
-    let pointer_state = pointers_state.get_or_insert_with(|| (HashMap::new(), 1));
-    let (pointers_map, next_pointer) = pointer_state;
+    let pointer_state = pointers_state.get_or_insert_with(PointerState::default);
+    let PointerState {
+        tick,
+        next_id,
+        id_map,
+    } = pointer_state;
+    *tick += 1;
+
+    eprintln!("Tick: {}", *tick);
     for pointer in pointers {
-        let id = match pointers_map.entry(pointer.dwID) {
-            Entry::Occupied(entry) => *entry.get(),
+        let id = match id_map.entry(pointer.dwID) {
+            Entry::Occupied(mut entry) => {
+                let (id, pointer_tick) = entry.get_mut();
+                *pointer_tick = *tick;
+                *id
+            }
             Entry::Vacant(entry) => {
-                let id = *next_pointer;
-                entry.insert(id);
-                *next_pointer += 1;
+                let id = *next_id;
+                entry.insert((id, *tick));
+                *next_id += 1;
                 id
             }
         };
 
         let x = (pointer.x - scaled_left) as f32 / scaled_width;
         let y = (pointer.y - scaled_top) as f32 / scaled_height;
+        eprintln!(
+            "id: {}, x: {}, y: {}, down: {}, move: {}, up: {}",
+            id,
+            x,
+            y,
+            pointer.dwFlags.0 & TOUCHEVENTF_DOWN.0 != 0,
+            pointer.dwFlags.0 & TOUCHEVENTF_MOVE.0 != 0,
+            pointer.dwFlags.0 & TOUCHEVENTF_UP.0 != 0
+        );
         if pointer.dwFlags.0 & TOUCHEVENTF_DOWN.0 != 0
             || pointer.dwFlags.0 & TOUCHEVENTF_MOVE.0 != 0
         {
@@ -149,10 +228,19 @@ fn handle_touch_event(message: &CWPSTRUCT) -> Result<(), EventError> {
             });
         }
         if pointer.dwFlags.0 & TOUCHEVENTF_UP.0 != 0 {
-            pointers_map.remove(&pointer.dwID);
+            id_map.remove(&pointer.dwID);
             queue.push_back(ProxyMessage::Remove { index: id });
         }
     }
+
+    id_map.retain(|_, (id, pointer_tick)| {
+        if *pointer_tick == *tick {
+            true
+        } else {
+            queue.push_back(ProxyMessage::Remove { index: *id });
+            false
+        }
+    });
 
     Ok(())
 }
