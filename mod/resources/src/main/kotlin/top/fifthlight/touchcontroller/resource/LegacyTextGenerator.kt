09c97f58b57960257abd9a64b0a35aade6c707de
fifth_light <liu-yu-jie@foxmail.com>
2025-01-20 23:21:06 +0800

    修复 1.12.2 文本换行问题

--- mod/resources/src/main/kotlin/top/fifthlight/touchcontroller/resource/LegacyTextGenerator.kt
+++ mod/resources/src/main/kotlin/top/fifthlight/touchcontroller/resource/LegacyTextGenerator.kt
@@ -4,7 +4,6 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
 import java.io.File
-import java.util.*
 
 @OptIn(ExperimentalSerializationApi::class)
 fun main(args: Array<String>) {
@@ -16,14 +15,9 @@ fun main(args: Array<String>) {
     for (file in languageFiles) {
         val outputFile = File(legacyLanguageDir, "${file.nameWithoutExtension}.lang")
         val map: Map<String, String> = Json.decodeFromStream(file.inputStream())
-        Properties().apply {
-            putAll(map)
-            store(outputFile.writer(), null)
-        }
-        val lines = outputFile.readLines().sorted().drop(1)
         outputFile.writer().use { writer ->
-            lines.forEach { line ->
-                writer.appendLine(line)
+            map.entries.sortedBy { (key, _) -> key }.forEach { (key, value) ->
+                writer.appendLine("$key=$value")
             }
         }
     }
